ebab39f51598412aba532dae52fd7fcb838d39ac
JustTrzcina <andrzej.maciaszek009@gmail.com>
2024-02-04 12:00:55 +0100

    generic decorators

--- Decorators/decorators.py
+++ Decorators/decorators.py
@@ -2,19 +2,38 @@ import functools
 
 user= {'username':"Drew44",'access_level':'admin'}
 
-def user_has_permission(func):
-    @functools.wraps(func)
-    def secure_func(panel):
-        if user.get('access_level') == 'admin':
-            return func(panel)
-    return secure_func
+def access_control(access_level):
+    def user_has_permission(func):
+        @functools.wraps(func)
+        def secure_func(*args,**kwargs):
+            if user.get('access_level') == access_level:
+                return func(*args,**kwargs)
+        return secure_func
+    return user_has_permission
         
-@user_has_permission
+@access_control('admin')
 def my_func(panel):
     return f'Password for {panel} panel = qwerty'
 
+def another_func():
+    pass
 
 print(my_func('movies'))
 print(my_func.__name__)
+print(another_func())
+print(another_func.__name__)
 
-my_func = user_has_permission(my_func)
+
+
+
+def add_all(*args):
+    return sum(args)
+
+print(add_all(6,3,3,2))
+
+def pretty_print(**kwargs):
+    for k,v in kwargs.items():
+        print(f'{k} -- {v}')
+
+pretty_print(**{"username":"andrew", "access_level":"guest"})
+
