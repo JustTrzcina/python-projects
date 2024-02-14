a54f40abb3c3f0016bad144236f634f2f3356527
JustTrzcina <andrzej.maciaszek009@gmail.com>
2024-02-14 12:47:19 +0100

    add tags to the store

--- FlaskStore/schemas.py
+++ FlaskStore/schemas.py
@@ -7,8 +7,11 @@ class PlainItemSchema(Schema):
 
 class PlainStoreSchema(Schema):
     name = fields.Str(required=True)
-    id = fields.Str(dump_only=True)
+    id = fields.Int(dump_only=True)
 
+class PlainTagSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str()
 
 class ItemUpdateSchema(Schema):
     name = fields.Str()
@@ -18,6 +21,18 @@ class ItemUpdateSchema(Schema):
 class ItemSchema(PlainItemSchema):
     store_id=fields.Int(required=True,load_only=True)
     store=fields.Nested(PlainStoreSchema(),dump_only=True)
+    tags=fields.List(fields.Nested(PlainTagSchema()),dump_only=True)
 
 class StoreSchema(PlainStoreSchema):
     items = fields.List(fields.Nested(PlainItemSchema()),dump_only=True)
+    tags = fields.List(fields.Nested(PlainTagSchema()),dump_only=True)
+
+class TagSchema(PlainTagSchema):
+    store_id=fields.Int(load_only=True)
+    store=fields.Nested(PlainStoreSchema(),dump_only=True)
+    items=fields.List(fields.Nested(PlainItemSchema()),dump_only=True)
+
+class TagAndItemSchema(Schema):
+    message = fields.Str()
+    item = fields.Nested(ItemSchema)
+    tag = fields.Nested(TagSchema)
