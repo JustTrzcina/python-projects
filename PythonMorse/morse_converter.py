617601ba8960d6ad473077a783ca9c6436ad90e5
JustTrzcina <andrzej.maciaszek009@gmail.com>
2024-02-20 18:57:11 +0100

    finish python morse converter

--- PythonMorse/morse_converter.py
+++ PythonMorse/morse_converter.py
@@ -1,4 +1,5 @@
 from typing import TypedDict,List
+from os import path
 
 MORSE_CODE = {
     'a': '.-', 
@@ -26,7 +27,8 @@ MORSE_CODE = {
     'w': '.--',
     'x': '-..-',
     'y': '-.--',
-    'z': '--..'
+    'z': '--..',
+    ' ': '/'
 }
 
 class MorseDict(TypedDict):
@@ -37,14 +39,15 @@ class MorseConverter():
     def __init__(self,conversion_table:MorseDict) -> None:
         self.conversion_table = conversion_table
         self.inverted_table = {v: k for k, v in MORSE_CODE.items()}
-
+        self.user_functions={'a':self.from_letters,'b':self.from_morse}
+        self.menu_text='Select the type of conversion:\na) To morse\nb) To text\nq) To quit\nEnter "a","b" or "q": '
 
     def _parse_file(self,path)-> List[str]:
         with open(path,mode='r') as f:
             text_data = f.readlines()
         words = []
         for line in text_data:
-            words.extend(line.strip().split())
+            words.extend(line.split())
         return words
     
     def from_letters(self,path):
@@ -53,17 +56,39 @@ class MorseConverter():
         for word in words:
             morse_word=[]
             for letter in word:
-                morse_word.append(self.conversion_table.get(letter.lower(),'_'))
+                morse_word.append(self.conversion_table.get(letter.lower(),'?'))
             morse_string = ' '.join(morse_word)
             results.append(morse_string)
         final_string = (' / '.join(results))
-        with open('result.txt','w') as f:
+        with open('result_morse.txt','w') as f:
             f.write(final_string)
 
-    def from_morse(self,morse_code):
-        return ''.join(self.inverted_table.get(code, '') for code in morse_code.split())
+    def from_morse(self,path):
+        words = self._parse_file(path)
+        final_string =  ''.join(self.inverted_table.get(code, '?') for code in words)
+        with open('result_text.txt','w') as f:
+            f.write(final_string)
 
+    def morse_run(self):
+        
+        user_input = input(self.menu_text)
+        while user_input!='q':
+            if user_input in self.user_functions:
+                operation = self.user_functions[user_input]
+                user_path = input('Specify the file path: ')
+                if not path.isfile(user_path):
+                    print(f'{user_path} does not exist')
+                else:
+                    try:
+                        operation(user_path)
+                    except Exception as e:
+                        print(f'An error occured during conversion:{e}')
+                    print('Conversion successful')
+                    operation(user_path)
+            else:
+                print('Invalid command')
+            user_input = input(self.menu_text)
 
-converter = MorseConverter(MORSE_CODE)
-converter.from_letters('sample.txt')
-print(converter.from_morse('.-.. --- -. --. / ... . -. - . -. -.-. .'))
+if __name__ =='__main__':
+    converter = MorseConverter(MORSE_CODE)
+    val = converter.morse_run()
--- README.md
+++ README.md
@@ -4,6 +4,9 @@ This is a repository full of projects that were done as a part of different cour
 
 ## Favourite projects
 
-- [FlaskStore](https://github.com/andrewmaci/python-projects/tree/main/FlaskStore) - RESTful store API created with Flask, Flask-smorest, SQLAlchemy and JWT tokens
+- [FlaskStore](https://github.com/andrewmaci/python-projects/tree/main/FlaskStore) - RESTful store API created with Flask, Flask-smorest, SQLAlchemy and JWT tokens. The site allows for creation of stores, items and tags.
 
-- [WebScraping](https://github.com/andrewmaci/python-projects/tree/main/WebScraping) - Two python scrapers implemented with **aiohttp** and **selenium** used on 'books.toscrape.com' and 'quotes.toscrape.com'
+- [WebScraping](https://github.com/andrewmaci/python-projects/tree/main/WebScraping) - Python scrapers implemented mainly with **aiohttp**, **selenium**, **beautifulsoup**.
+
+- [PythonMorse](https://github.com/andrewmaci/python-projects/tree/main/PythonMorse) - Side project created to test ChatGPT3.5 if it is capable of cummunicating with morse code. Instead of using pre-existing converters I decided to create my own. **Conclusions** - During testing ChatGPT3.5 was mostly capable of comprehending sent messages but struggled with responses, spewing gibberish and sometimes locking itself in a loop sending the same message. Simillar behaviour could be observed with Bard.
+ 
